ef98d2cabb2bd99e9c63ad7d88536c9bb33fe923
Yogita Agawal <Yogita@coditation.com>
2025-08-05 12:30:51 +0530

    pagination component

--- src/components/UI/Pagination.jsx
+++ src/components/UI/Pagination.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
-const Pagination = () => {
+const Pagination = ({ page, setPage }) => {
     return (
-        <div>Pagination</div>
+        <div className='flex justify-center gap-4 mt-6'>
+            <button onClick={setPage((p) => Math.max(p - 1, 1))}
+                disabled={page === 1}
+                className='px-4 py-2 bg-gray-800 rounded disabled:opacity-50'
+            >
+                Prev
+
+            </button>
+            <button onClick={setPage((p) => p + 1)}
+                disabled={page === 1}
+                className='px-4 py-2 bg-gray-800 rounded'
+            >
+                Next
+            </button>
+        </div>
     )
 }
 
